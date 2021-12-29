2c34c868043ce4413466d4a26cad4c4dc6ed4b68
Bruce Choe <whdtmek@gmail.com>
2021-12-29 15:11:26 +0900

    공유기능 1차 적용(설치링크 공유)

--- components/DetailsScreen/HeaderRight.js
+++ components/DetailsScreen/HeaderRight.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  Share,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const HeaderRight = (props) => {
+  const link =
+    Platform.OS === 'ios'
+      ? 'https://apps.apple.com/us/app/gem%EC%A0%AC/id1576464783'
+      : 'https://play.google.com/store/apps/details?id=com.brucecompany.gem';
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: link,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log('acitivtyType!');
+        } else {
+          console.log('Share!');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log('dismissed');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.shareBTContainer} onPress={() => {}}>
+      <TouchableOpacity
+        style={styles.shareBTContainer}
+        onPress={() => {
+          onShare();
+        }}
+      >
         <Ionicons name='share-outline' size={26} color='black' />
       </TouchableOpacity>
       <TouchableOpacity style={styles.bookmarkBTContainer} onPress={() => {}}>
